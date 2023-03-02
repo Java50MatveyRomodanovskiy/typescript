40306d8f1b92008cc17ea07f956cc9b0368d5952
MatveyRomodanovskiy <startpolus@gmail.com>
2023-03-03 00:07:43 +0300

    final

--- src/CipherShiftLetter.ts
+++ src/CipherShiftLetter.ts
@@ -1,6 +1,6 @@
 export function shiftLetter(letter : string, shift: number) : string{
     const aCode = ' '.charCodeAt(0)
     let offset = letter.charCodeAt(0) - aCode;
-    let newPosition = (aCode +(offset + shift) % 95); 
+    let newPosition = (aCode +(offset + shift) % ('~'.charCodeAt(0) - ' '.charCodeAt(0) + 1)); 
     return String.fromCharCode(newPosition );
 }
