a63b0a255d78a1b5f8d94a09975bfdb183a8ac64
MatveyRomodanovskiy <startpolus@gmail.com>
2023-03-03 00:13:13 +0300

    final

--- src/CipherShiftRound.ts
+++ src/CipherShiftRound.ts
@@ -1,7 +1,7 @@
 import { shiftLetter } from "./CipherShiftLetter"
 export function shiftRound(str: string, shift: number): string {
-  const delimeter = '~'.charCodeAt(0) - ' '.charCodeAt(0) + 1;
-    shift = (delimeter + shift % delimeter) % delimeter // normalize any positive or negative shift to be always in border
+  const devider = '~'.charCodeAt(0) - ' '.charCodeAt(0) + 1;
+    shift = (devider + shift % devider) % devider // normalize any positive or negative shift to be always in border
     return str
       .split("")
       .map(symbol => shiftLetter(symbol, shift))
